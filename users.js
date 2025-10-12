39e0ab32a6d2b7f554816b5c6f76851b5540b38d
Reliance001 <127873544+Reliance001@users.noreply.github.com>
2025-10-12 20:46:31 +0100

    Update users.js
    MongoDB string changed

--- users.js
+++ users.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://Adedayo:Adedayo12154@projects.8j2dxit.mongodb.net/Projects?retryWrites=true&w=majority&appName=Projects"
+    "mongodb+URL"
   )
   .then(async () => {
     const admin = new User({
-      name:"Adedayo",
-      email: "admin@test.com",
-      password: "Arshcon2025",
+      name:"",
+      email: "",
+      password: "",
       role: "admin",
     });
     await admin.save();
     console.log("Admin user created!");
     process.exit();
   });
+
